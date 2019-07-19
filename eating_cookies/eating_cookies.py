b6391d4657db8c7ece66ab1cf62e2fcfdf1a7c3c
Sharanjit Sandhu <sharanjit.sandhuu@gmail.com>
2019-07-18 21:04:39 -0700

    Solved Eating cookies problem

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -3,14 +3,36 @@
 import sys
 
 # The cache parameter is here for if you want to implement
-# a solution that is more efficient than the naive 
+# a solution that is more efficient than the naive
 # recursive solution
-def eating_cookies(n, cache=None):
-  pass
 
-if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_cookies = int(sys.argv[1])
-    print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
-  else:
-    print('Usage: eating_cookies.py [num_cookies]')
+
+def eating_cookies(n, cache=None):
+  # initializing the base cases
+    cache = {
+        0: 1,
+        1: 1,
+        2: 2,
+        3: 4
+    }
+    # base case to handle negative number
+    if n <= 1:
+        return 1
+
+    if n in cache:
+        return cache[n]
+
+    cache[n] = eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+
+    return cache[n]
+
+
+print(eating_cookies(5))
+
+# if __name__ == "__main__":
+#     if len(sys.argv) > 1:
+#         num_cookies = int(sys.argv[1])
+#         print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(
+#             ways=eating_cookies(num_cookies), n=num_cookies))
+#     else:
+#         print('Usage: eating_cookies.py [num_cookies]')
