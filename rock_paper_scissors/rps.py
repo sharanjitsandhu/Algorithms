bd39113ab801d712defb77795af57b2f44e8ad44
Sharanjit Sandhu <sharanjit.sandhuu@gmail.com>
2019-07-18 22:39:03 -0700

    Answered rock paper scissors problem

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,13 +2,30 @@
 
 import sys
 
+
 def rock_paper_scissors(n):
-  pass 
+    answer = []
+    choices = ["rock", "paper", "scissors"]  # possible choices
+
+    # inner recursive function to add result
+    def get_result(pending_rounds, result=[]):
+        # base case: no rounds remain
+        if pending_rounds == 0:
+            answer.append(result)
+            return
+        for choice in choices:
+            get_result(pending_rounds - 1, result + [choice])
+
+    get_result(n)
+    return answer
+
+
+rock_paper_scissors(12)
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
